3b6da594af5c69186c82ce7da99911d6db5935b6
Ahsan <ahsan.sibghatullah49@gmail.com>
2017-11-14 08:52:02 +0700

    navigate react with reat-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './Components/Pages/HomePage';
+import LoginPage from './Components/Pages/LoginPage';
 
-const App = () => <div>Hello World!</div>
+const App = () => {
+    return (
+    <div>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+    </div>);
+}
 
 export default App;
